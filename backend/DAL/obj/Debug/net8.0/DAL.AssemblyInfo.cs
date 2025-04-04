47f101cf28bc57d04bc369a59647336bf2891e69
Pablo Alvarado Umaña <pablo.alvarado.umana@est.una.ac.cr>
2025-04-04 08:14:12 -0600

    Plantilla

--- backend/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ backend/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a551d289df791bd8e5cdbb1aaa9879efdd42cd87")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
